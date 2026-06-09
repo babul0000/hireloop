2d29b1c405fa516227455ac53fe243ddbbbc111e
Your Name <babulhossan.info@gmail.com>
2026-06-09 23:25:58 +0600

    pricing setup strip setup and complete desgin

--- src/lib/stripe.js
+++ src/lib/stripe.js
@@ -3,3 +3,10 @@ import 'server-only'
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+
+export const PLAN_PRICE_ID = {
+    'seeker_pro': 'price_1TgEElAlomi13zcXMGwN1JCj',
+    'seeker_premium': 'price_1TgRjaAlomi13zcXlKLXtacC',
+    'recruiter_enterprise': 'price_1TgRkKAlomi13zcXMsBcYgb6',
+    'recruiter_growth': 'price_1TgRkqAlomi13zcXQWUhfoEU'
+}
